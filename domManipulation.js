73e1433798425a5a3fda36f128f380cf59e23315
valeriem76 <146957901+valeriem76@users.noreply.github.com>
2024-01-31 14:54:07 +0000

    completed all tasks raahhhh

--- domManipulation.js
+++ domManipulation.js
@@ -9,17 +9,17 @@ let row1 = document.querySelector(".row-1");
 // CHALLENGE #1 - make the rows match
 
 // 1. Set the styling of `example1` to a background color of black.
-
+example1.style.backgroundColor = "black";
 // 2. Set the styling of `pink` to a background color of pink.
-
+pink.style.backgroundColor = "pink";
 // 3. Set the styling of `green` to a background color of green.
-
+green.style.backgroundColor = "green";
 // 4. Set the styling of `blue` to a background color of blue.
-
+blue.style.backgroundColor = "green";
 // 5. Set the styling of `lightGreen` to a background color of lightGreen.
-
+lightGreen.style.backgroundColor = "lightGreen";
 // 6. Set the styling of `row1` to have a font color of white.
-
+row1.style.color = "white";
 
 
 // CHALLENGE #2 - style the image
@@ -29,9 +29,10 @@ let imageButton = document.querySelector(".image-button");
                                    
 // 7. Add an event listener to `imageButton`
 // - The event type should be "click" or "dblclick"
-
 // Include the next tasks in the body of the function.
-
+imageButton.addEventListener("click", function(){
+  image.style.width = "300px";
+})
 // 8. The image is wayyyy too big and needs a border.
 // - Set the width of `image` to 300px
 // - Set the border of `image` so that it's:
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let img = document.querySelector("img");
 // - Make it so that the colors are different than what's used.
 // - Try using a hex color code.
 button.addEventListener("click", function() {
-    body.style.backgroundColor = "black";
+    body.style.backgroundColor = "darkblue";
     header.style.color = "white";
     img.style.border = "5px solid white";
 });
